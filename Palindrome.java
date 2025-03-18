6f7c782a1c69aa2465a5b381abb7be312ade3b3a
Kumaraguru <127617090+GURU-KG@users.noreply.github.com>
2025-03-18 21:06:22 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -2,14 +2,14 @@ class Solution {
     public boolean isPalindrome(int x) {
 		int digit = 0;
 		int count = 0;
-        int res = 0;
+        int result = 0;
         int temp = x;
 		while(x>0) {
 			digit = x % 10;
-			res = (res*10)+digit;
+			result = (result*10)+digit;
 			x = x / 10;
 		}
-		if(temp == res) {
+		if(temp == result) {
 			return(true);
 		}
 		else {
